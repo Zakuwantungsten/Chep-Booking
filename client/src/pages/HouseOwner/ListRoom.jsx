a87ebbbd1cd9c25d192ef284252efa4c5c76618a
Zakuwantungsten <zakuwantungsten@gmail.com>
2025-10-08 20:58:02 +0300

    making the dashboard

--- client/src/pages/HouseOwner/ListRoom.jsx
+++ client/src/pages/HouseOwner/ListRoom.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {roomsDummyData} from '../../assets/assets'
 
 const ListRoom = () => {
+
+  const [rooms, setRooms] = useState(roomsDummyData)
+
+
   return (
     <div>
         
